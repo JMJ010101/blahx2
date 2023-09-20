ca568da4d2b0a2e5d56a77cc9c6d1965305bbf09
JMJ010101 <wjdalswn218@naver.com>
2023-09-20 22:38:55 +0900

    사용자 정보 조회 API 추가

--- controllers/error/handle_error.ts
+++ controllers/error/handle_error.ts
@@ -7,13 +7,16 @@ const handleError = (err: unknown, res: NextApiResponse) => {
   let unknownErr = err;
   //에러 할당
   if (err instanceof CustomServerError === false) {
+    // 에러가 instanceof로 CustomServerError인지 확인 / 예외처리로 꺼내는 게 편함 => 이게 아닐 경우에 다른 처리를 하는 것이 편함
     unknownErr = new CustomServerError({ statusCode: 499, message: 'unknown error' });
+    // false일 경우 CustomServerError를 새로 발생시킴. 임의로 알 수 없는 499라고 표시
   }
   const customError = unknownErr as CustomServerError;
+  // 타입캐스팅을 해서 바로 사용
   res
     .status(customError.statusCode)
     .setHeader('location', customError.location ?? '')
-    .send(customError.serializeErrors());
+    .send(customError.serializeErrors()); // 에러 응답에 body를 전달
 };
 
 export default handleError;
